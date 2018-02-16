49daecf01530078c7b9d83b3760ab2da7aad5772
Andrew Ward <tmiandreww@gmail.com>
2018-02-15 20:45:29 -0500

    restructure window controls class

--- js/window-controls.js
+++ js/window-controls.js
@@ -2,6 +2,20 @@
 class WindowControls {
   constructor(jquery) {
     this.$ = jquery
+    if(process.platform != 'darwin') {
+      $('#window-controls-darwin').remove()
+      $('#controlsNav>ul').append(`
+        <li class="nav-item ml-auto">
+          <a class="nav-link window-control" href="#" id="minimize"><svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="./icons/windows.svg#minimize-window"></use></svg></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link window-control" href="#" id="maximize"><svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="./icons/windows.svg#maximize-window"></use></svg></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link window-control" href="#" id="quit"><svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="./icons/windows.svg#close-window"></use></svg></a>
+        </li>
+      `)
+    }
   }
 
   start() {
@@ -32,21 +46,6 @@ class WindowControls {
     $(document).on('click', '#minimize', () => {
       remote.getCurrentWindow().minimize()
     })
-
-    if(process.platform != 'darwin') {
-      $('#window-controls-darwin').remove()
-      $('#controlsNav>ul').append(`
-        <li class="nav-item ml-auto">
-          <a class="nav-link window-control" href="#" id="minimize"><svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="./icons/windows.svg#minimize-window"></use></svg></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link window-control" href="#" id="maximize"><svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="./icons/windows.svg#maximize-window"></use></svg></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link window-control" href="#" id="quit"><svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="./icons/windows.svg#close-window"></use></svg></a>
-        </li>
-      `)
-    }
   }
 }
 
